e4d3434b82fa3166ff48fe8496983f36af52dfc8
Thailam29 <lamntph34492@fpt.edu.vn>
2023-05-17 15:01:12 +0700

    LamNT

---
+++ app/src/main/java/com/example/lab3_ph34455/relativelayout.java
@@ -0,0 +1,14 @@
+package com.example.lab3_ph34455;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class relativelayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relativelayout);
+    }
+}
